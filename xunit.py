c0bf0ae5f05d35ad8ad0435e4f16b7e149443be2
Mutsumi Sugizaki <m-sugizaki@cubesystem.co.jp>
2019-11-05 15:11:19 +0900

    18-2

--- xunit.py
+++ xunit.py
@@ -1,4 +1,10 @@
+class WasRun:
+	def __init__(self, name):
+		self.wasRun = None
+	def testMethod(self):
+		self.wasRun = 1
+
 test = WasRun("testMethod")
 print(test.wasRun)
 test.testMethod()
-print(testwasRun)
+print(test.wasRun)
